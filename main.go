dbacffaaf7d31ea2fbdd6a9c2b312dde22ac1e47
manfered <farzad.seifi@gmail.com>
2022-02-06 13:02:45 -0700

    Declaration of an slice without the underlying array management and appending items to the slice

--- main.go
+++ main.go
@@ -17,4 +17,13 @@ func main() {
 	// changes on the slice will be reflected on the underlying array
 	integerSlice[1] = 22
 	fmt.Println(integerArray)
+
+	// declaration of an slice without the underlying array
+	floatSlice := []float32{1.1, 2.2, 3.3}
+	fmt.Println(floatSlice)
+
+	// appending items to the slice in order to present the dynamic size of a slice
+	floatSlice = append(floatSlice, 4.4, 5.5, 6.6)
+	fmt.Println(floatSlice)
+
 }
